8d50ba84d4b977617ee958e5b8cd5899093e7afe
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-09-25 23:34:22 +0530

    Create Number of Unique Paths.cpp

---
+++ Number of Unique Paths.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,m;
+	    cin>>n>>m;
+	    int** arr=new int*[n];
+	    for(int i=0;i<n;i++){
+	        arr[i]=new int[m];
+	    }
+	    
+	    for(int i=0;i<n;i++){
+	        arr[i][0]=1;
+	    }
+	    for(int i=0;i<m;i++){
+	        arr[0][i]=1;
+	    }
+	    for(int i=1;i<n;i++){
+	        for(int j=1;j<m;j++){
+	            arr[i][j]=arr[i][j-1]+arr[i-1][j];
+	        }
+	    }
+	    cout<<arr[n-1][m-1]<<endl;
+	    for(int i=0;i<n;i++){
+	        delete[]  arr[i];
+	    }
+	    delete[] arr;
+	    
+	}
+	return 0;
+}
