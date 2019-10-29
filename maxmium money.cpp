b1aaa4bc8f718a5a5f462678f743efe0aadac32e
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-10-29 12:13:57 +0530

    Create maxmium money.cpp

---
+++ maxmium money.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+ {
+     float i,j,k,n,t;
+     cin>>t;
+     while(t--)
+     {
+         cin>>i>>j;
+         k=ceil(i/2);
+         cout<<k*j<<endl;
+     }
+	//code
+	return 0;
+}
