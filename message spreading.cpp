40b1218ae728b206c57e859b6ccef1cf228ad6c5
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-11-03 22:38:22 +0530

    Create message spreading.cpp

---
+++ message spreading.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int i;
+         cin>>i;
+         cout<<(i-1)*2<<endl;
+     }
+	//code
+	return 0;
+}
