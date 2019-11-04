48704020abda99501c813b68a2b58ff7f305cdcf
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-11-04 23:19:13 +0530

    Create doctor's clinic.cpp

---
+++ doctor's clinic.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int i,j;
+         cin>>i>>j;
+         cout<<((i-1)*10)-j*(i-1)<<endl;
+     }
+	//code
+	return 0;
+}
