f9d27ba659d51eb9f7d3bc8581616df14f511ea2
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-11-05 21:15:55 +0530

    Create geek and coffee chop.cpp

---
+++ geek and coffee chop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+#include <math.h>
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         long long n;
+         int m;
+         cin>>n>>m;
+         int d=pow(2,m-1);
+         cout<<n/d<<endl;
+     }
+	//code
+	return 0;
+}
