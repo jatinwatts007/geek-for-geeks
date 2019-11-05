118d62d8738528db6740f0c7d2b65c72566f9f8e
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-11-05 21:14:07 +0530

    Create Anshuman's Favourite Number.cpp

---
+++ Anshuman's Favourite Number.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int i;
+         cin>>i;
+         if(i%5==0)
+         {
+            cout<<"YES"<<endl; 
+         }else
+         {
+             cout<<"NO"<<endl;
+         }
+     }
+	//code
+	return 0;
+}
