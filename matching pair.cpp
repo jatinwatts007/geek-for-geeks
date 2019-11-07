0c92b5f9a1cf1f6333dc0ab98f9011c4f45a42d2
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-11-07 22:26:06 +0530

    Create matching pair.cpp

---
+++ matching pair.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int i;
+         cin>>i;
+         cout<<i+1<<endl;
+     }
+	//code
+	return 0;
+}
