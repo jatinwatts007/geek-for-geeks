0dae5c5e658890d599e93f7d3619d04ef754a8e3
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-11-08 21:35:49 +0530

    Create the lazy caterer's problem.cpp

---
+++ the lazy caterer's problem.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         long long n;
+        cin>>n;
+        long long s=n*(n+1)/2;
+        cout<<s+1<<endl;
+     }
+	//code
+	return 0;
+}
