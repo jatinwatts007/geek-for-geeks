f6c3e31c7c3c9efe4ce0a0e28e94319cd4c47b55
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-07-27 16:04:44 +0530

    Create pattern print.cpp

---
+++ pattern print.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--){
+         int i,j;
+         cin>>i;
+         j=i;
+         while(i>0){
+             cout<<i<<" ";
+             i=i-5;
+         }
+         while(i!=j){
+             cout<<i<<" ";
+             i=i+5;
+         }
+         cout<<j<<endl;
+     }
+	//code
+	return 0;
+}
