230f42c736fde73a28418f6188ca8c5abb64c52b
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-07-27 12:20:47 +0530

    Create Pattern Jumping.cpp

---
+++ Pattern Jumping.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+bool isPowerOfTwo(int n)  
+{  
+    if (n == 0)  
+        return 0;  
+    while (n != 1)  
+    {  
+        if (n%2 != 0)  
+            return 0;  
+        n = n/2;  
+    }  
+    return 1;  
+} 
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--){
+         int i;
+         cin>>i;
+         isPowerOfTwo(i)? cout<<"True\n": cout<<"False\n";  
+     }
+    
+	//code
+	return 0;
+}
