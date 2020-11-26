ea89657a2f8419f85fda2db7f22347eb271007b5
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-11-26 23:42:08 +0530

    Create Half N by M.cpp

---
+++ Half N by M.cpp
@@ -0,0 +1,15 @@
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         long long n;
+         int m;
+         cin>>n>>m;
+         int d=pow(2,m-1);
+         cout<<n/d<<endl;
+     }
+	//code
+	return 0;
+}
