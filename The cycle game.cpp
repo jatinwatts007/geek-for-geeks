176a9626b09e0c30d045609a6ce4f71f66e49d1d
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-11-28 23:42:40 +0530

    Create The cycle game.cpp

---
+++ The cycle game.cpp
@@ -0,0 +1,24 @@
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int x,y,n;
+         cin>>x>>y>>n;
+         int j=n/2;
+         int k=n-j;
+         x=x*pow(2,k-j);
+         
+         if(x>y)
+         {
+             cout<<x/y<<endl;
+         }else{
+             cout<<y/x<<endl;
+         }
+         
+         
+     }
+	//code
+	return 0;
+}
