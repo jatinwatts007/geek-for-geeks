136f16339e3b19c951171423a5052b2c9ad0cc1e
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-12-30 22:17:46 +0530

    Create Armstrong Numbers .cpp

---
+++ Armstrong Numbers .cpp
@@ -0,0 +1,36 @@
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+// User function Template for C++
+class Solution {
+  public:
+    string armstrongNumber(int n){
+        int sum=0,num=n;
+        while(n>0)
+        {
+            sum=sum+pow(n%10,3);
+            n=n/10;
+        }
+        if(sum==num) return "Yes";
+        else return"No";
+        // code here
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.armstrongNumber(n) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
