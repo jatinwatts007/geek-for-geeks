af37ce0efbe728dd12fe0010efa5b86340f1edf8
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2021-05-01 23:54:23 +0530

    Create Make diamonds.cpp

---
+++ Make diamonds.cpp
@@ -0,0 +1,46 @@
+using namespace std;
+
+int deathCircle(int n, int k)
+{
+   //Your code here
+   if(k==0) return 1;
+   int arr[n];
+   for(int i=0;i<n;i++)
+   {
+       arr[i]=i+1;
+   }
+   int i=0;
+   int x;
+   int count=0;
+   int count1=0;
+   while(count1<n)
+   {
+       count=0;
+       while(count!=k)
+       {
+           if(i==n)i=0;
+           if(arr[i]!=0)count++;
+           if(count==k)break;
+           i++;
+           
+       }
+       x=arr[i];
+       arr[i]=0;
+       count1++;
+       i++;
+   }
+   return x;
+}
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int n,k;
+         cin>>n>>k;
+         cout<<deathCircle(n,k)<<endl;
+     }
+	//code
+	return 0;
+}
